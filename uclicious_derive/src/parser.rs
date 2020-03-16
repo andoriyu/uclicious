a2f3edef98e2db98599aa2c2232aaee957341f40
Andrey Cherkashin <with.out@me.com>
2020-03-15 17:03:15 -0700

    feat: Ability to register variables on a builder.

--- uclicious_derive/src/parser.rs
+++ uclicious_derive/src/parser.rs
@@ -38,11 +38,28 @@ impl ToTokens for ParserMethods {
         let err = bindings::ucl_parser_error();
         let path = bindings::path_ty();
         tokens.append_all(quote! (
+        /// Add a chunk of text to the parser. String must:
+        /// - not have `\0` character;
+        /// - must be valid UCL object;
         #vis fn add_chunk_full<C: #as_ref<str>>(&mut self, chunk: C, priority: #priority, strategy: #dup_strategy) -> #result<(), #err> {
             self.__parser.add_chunk_full(chunk, priority, strategy)
         }
+        /// Add a file by a file path to the parser. This function uses mmap call to load file, therefore, it should not be shrunk during parsing.
         #vis fn add_file_full<F: #as_ref<#path>>(&mut self, file: F, priority: #priority, strategy: #dup_strategy) -> #result<(), #err> {
             self.__parser.add_file_full(file, priority, strategy)
+        }
+        /// Register new variable `$var` that should be replaced by the parser to the `value` string.
+        /// Variables need to be registered _before_ they are referenced.
+        ///
+        /// #### Panics
+        /// This function panics if either `var` or `value` has `\0`.
+        #vis fn register_variable<K: #as_ref<str>, V: #as_ref<str>>(
+            &mut self,
+            var: K,
+            value: V,
+        ) -> &mut Self {
+            self.__parser.register_variable(var, value);
+            self
         }
         ))
     }
