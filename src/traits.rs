73e69e8dd437957bbfc955abe71b211478921901
Andrey Cherkashin <with.out@me.com>
2020-03-21 16:39:55 -0700

    feat: Add TryInto trait for simplicity of conversion.

--- src/traits.rs
+++ src/traits.rs
@@ -6,3 +6,14 @@ pub trait FromObject<T>: Sized {
     /// Performs the conversion.
     fn try_from(value: T) -> Result<Self, ObjectError>;
 }
+
+pub trait TryInto<T> :Size {
+    fn try_into(self) -> Result<Self, ObjectError>;
+}
+
+/// A clone of TryInto trait from std.
+impl<T,U> TryInto<U> for T where U: FromObject<T> {
+    fn try_into(self) -> Result<Self, ObjectError> {
+        FromObject::try_from(self)
+    }
+}
